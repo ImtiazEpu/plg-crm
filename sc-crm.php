4db3f70c9260ca53b4ac4fae7625a142926595ed
ImtiazEpu <imtiazepu@programmer.net>
2019-10-16 15:45:43 +0600

    initial commit

---
+++ sc-crm.php
@@ -0,0 +1,20 @@
+<?php
+	/*
+	Plugin Name: Sourcecypher CRM
+	Plugin URL:
+	Description: A Custom CRM plugin with custom role & capabilities. 
+	Version: 1.0
+	Author: SourceCypher
+	Author URI: sourcecypher.net
+	License: GPLv2 or later
+	Text Domain: sc-crm
+	Domain Path: /languages/
+	*/
+	
+	class ScCrm {
+		
+		public function __construct(  ) {
+			
+		}
+	}
+	new ScCrm();
