ff26d282f0349c656d5ebc3db35567be39ba2bff
ImtiazEpu <imtiazepu@programmer.net>
2019-10-16 16:09:55 +0600

    register custom post type with post type capabilities

--- sc-crm.php
+++ sc-crm.php
@@ -13,8 +13,81 @@
 	
 	class ScCrm {
 		
+		/**
+		 * ScCrm constructor.
+		 */
 		public function __construct(  ) {
-			
+			add_action('init', array($this, 'sc_register_custom_post_type'));
+			add_action('plugins_loaded', array($this,'sc_load_text_domain'));
 		}
+		//End method construct
+		
+		/**
+		 * Registers a Custom Post Type called contact
+		 */
+		public function sc_register_custom_post_type(  ) {
+			register_post_type('contact', array(
+				'labels' => array(
+					'name'               => _x( 'Contacts', 'post type general name', 'tuts-crm' ),
+					'singular_name'      => _x( 'Contact', 'post type singular name', 'tuts-crm' ),
+					'menu_name'          => _x( 'Contacts', 'admin menu', 'tuts-crm' ),
+					'name_admin_bar'     => _x( 'Contact', 'add new on admin bar', 'tuts-crm' ),
+					'add_new'            => _x( 'Add New', 'contact', 'tuts-crm' ),
+					'add_new_item'       => __( 'Add New Contact', 'tuts-crm' ),
+					'new_item'           => __( 'New Contact', 'tuts-crm' ),
+					'edit_item'          => __( 'Edit Contact', 'tuts-crm' ),
+					'view_item'          => __( 'View Contact', 'tuts-crm' ),
+					'all_items'          => __( 'All Contacts', 'tuts-crm' ),
+					'search_items'       => __( 'Search Contacts', 'tuts-crm' ),
+					'parent_item_colon'  => __( 'Parent Contacts:', 'tuts-crm' ),
+					'not_found'          => __( 'No contacts found.', 'tuts-crm' ),
+					'not_found_in_trash' => __( 'No contacts found in Trash.', 'tuts-crm' ),
+				),
+				// Front-end
+				'has-archive' => false,
+				'public' =>false,
+				'publicly_queryable' =>false,
+				
+				// Admin
+				'capabilities' => array(
+					'edit_others_posts'     => 'edit_others_contacts',
+					'delete_others_posts'   => 'delete_others_contacts',
+					'delete_private_posts'  => 'delete_private_contacts',
+					'edit_private_posts'    => 'edit_private_contacts',
+					'read_private_posts'    => 'read_private_contacts',
+					'edit_published_posts'  => 'edit_published_contacts',
+					'publish_posts'         => 'publish_contacts',
+					'delete_published_posts'=> 'delete_published_contacts',
+					'edit_posts'            => 'edit_contacts'   ,
+					'delete_posts'          => 'delete_contacts',
+					'edit_post'             => 'edit_contact',
+					'read_post'             => 'read_contact',
+					'delete_post'           => 'delete_contact',
+				),
+				
+				'map_meta_cap' => true,
+				'menu_icon' => 'dashicons-businessman',
+				'menu_position' => 10,
+				'query_var' => true,
+				'show_in_menu' => true,
+				'show_ui' => true,
+				'supports' => array(
+					'title',
+					'author',
+					'comments',
+				),
+			));
+		}
+		// End method sc_register_custom_post_type
+		
+		/**
+		 * Load text domain
+		 */
+		public function sc_load_text_domain(  ) {
+			load_plugin_textdomain('sc-crm', false, dirname(__FILE__)."/languages");
+		}
+		//End method sc_load_text_domain
+		
+		
 	}
 	new ScCrm();
